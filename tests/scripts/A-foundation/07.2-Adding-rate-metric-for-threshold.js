6ef074f3a0093c4383b0a5aac299a7f0b12c52ad
Marc Vantveer <marc.vantveer@polteq.com>
2024-03-05 14:41:12 +0100

    added extra threshold rate test exercise

---
+++ tests/scripts/A-foundation/07.2-Adding-rate-metric-for-threshold.js
@@ -0,0 +1,30 @@
+import http from "k6/http";
+import { check } from "k6";
+import { Rate } from "k6/metrics";
+
+/*
+- split up the two checks into two separate checks and add them to the error rate variable
+ */
+
+// define a new rate metric
+export let errorRate = new Rate('errors')
+
+// define options with the thresholds
+export let options = {
+    thresholds: {
+        errors: ['rate<0.1'] // less than 10% errors otherwise execution stops
+    }
+}
+
+export default function (){
+    let response = http.get('https://www.polteq.com');
+
+    console.log(`Response status: ${response.status}, body size: ${Math.round(response.body.length/1024).toFixed(2)} MiB`)
+
+    let results = check(response, {
+        'is response status 200:': (r) => r.status === 200,
+        'body size is less than 70.000 bytes:' : (r) => r.body.length <= 70000  // home page of the Polteq website should be smaller than ≈ 70 Mb
+    });
+
+    errorRate.add(!results) // error rate is boolean value, if results from checks are OK, than error is not true. That's why there is a !
+}
