e6cc40d161caac00c915821d80e77c55c8941f9c
Marc Vantveer <marc.vantveer@polteq.com>
2024-03-07 19:48:45 +0100

    added extra header api exercise

--- tests/scripts/A-foundation/10.1-add-header-to-a-request.js
+++ tests/scripts/A-foundation/10.1-add-header-to-a-request.js
@@ -1,4 +1,5 @@
 import http from "k6/http";
+import { check } from "k6";
 
 /*
 - what headers can be used, see https://test-api.k6.io/public/crocodiles/?format=api
@@ -11,11 +12,17 @@ export default function (){
     let headerParam = {
         headers: {
             'Content-Type': 'application/json',
-            //'Accept': 'text/html'
+            // 'Accept': 'text/html' // see more info at https://developer.mozilla.org/en-US/docs/Glossary/Request_header
         }
     }
 
     let respons = http.get(url,headerParam);
+     let body = JSON.parse(respons.body)
     console.log(respons.request)
-    console.log(respons.body)
+    console.log(`Number of crocodiles: ${body.length}, First element: ${body[1].name}`)
+
+    check(respons, {
+        'is status is 200: ' : (r) => r.status === 200,
+        'message body is not empty: ' : (r) => r.body.length !== 0,
+    });
 }
